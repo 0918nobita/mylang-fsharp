735e7d4c408f9394341dd0ee505bf510cb3e23ad
Kodai Matsumoto <nobita.0918@gmail.com>
2023-08-06 22:15:35 +0900

    :sparkles: Implement `Parser.map/optional`

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -6,12 +6,36 @@ type Location = {
 }
 
 module Location =
+    let root = {
+        Line = 0
+        Column = 0
+    }
+
     let nextColumn (location: Location) : Location =
         {
             Line = location.Line
             Column = location.Column + 1
         }
+    
+    let nextLine (location: Location) : Location =
+        {
+            Line = location.Line + 1
+            Column = 0
+        }
 
+    let carriageReturn (location: Location) =
+        {
+            Line = location.Line
+            Column = 0
+        }
+
+    let lineFeed (location: Location) =
+        {
+            Line = location.Line + 1
+            Column = location.Column
+        }
+
+/// `let`
 type LetKeyword = {
     Location: Location
 }
@@ -21,6 +45,7 @@ type Identifier = {
     Raw: string
 }
 
+/// `=`
 type Equal = {
     Location: Location
 }
@@ -44,26 +69,104 @@ type VariableDeclaration = {
 type Statement =
     | VariableDeclaration of VariableDeclaration
 
-type ParserInput = {
-    Location: Location
+type ParserInput<'C> = {
+    Context: 'C
     Target: string
 }
 
-type ParseSuccess<'T> = {
-    Location: Location
+type ParseSuccess<'C, 'T> = {
+    Context: 'C
     ParsedValue: 'T
     Rest: string
 }
 
-type ParseError<'E> = {
-    Location: Location
-    Message: string
+type ParseError<'C, 'E> = {
+    Context: 'C
+    Reason: 'E
+    /// 飛ばして続きをパースする場合に入力となる文字列
     Rest: string
 }
 
-type ParseResult<'T, 'E> = Result<ParseSuccess<'T>, ParseError<'E>>
+type ParseResult<'C, 'T, 'E> = Result<ParseSuccess<'C, 'T>, ParseError<'C, 'E>>
 
-type Parser<'T, 'E> = ParserInput -> ParseResult<'T, 'E>
+type Parser<'C, 'T, 'E> = ParserInput<'C> -> ParseResult<'C, 'T, 'E>
+
+module Parser =
+    let map (mapping: 'T -> 'U) (parse: Parser<'C, 'T, 'E>) : Parser<'C, 'U, 'E> =
+        fun parserInput ->
+            parse parserInput
+            |> Result.map (fun parseSuccess ->
+                {
+                    Context = parseSuccess.Context
+                    ParsedValue = mapping parseSuccess.ParsedValue
+                    Rest = parseSuccess.Rest
+                })
+
+    let optional (parse: Parser<'C, 'T, 'E>) : Parser<'C, Option<'T>, 'E> =
+        fun { Context = context; Target = target } ->
+            match parse { Context = context; Target = target } with
+            | Ok { Context = context'; ParsedValue = parsedValue; Rest = rest } ->
+                Ok {
+                    Context = context'
+                    ParsedValue = Some parsedValue
+                    Rest = rest
+                }
+            | Error _ ->
+                Ok {
+                    Context = context
+                    ParsedValue = None
+                    Rest = target
+                }
+
+type LineBreak = Crlf | Lf
+
+let charParser (c : char) : Parser<LineBreak * Location, char, string> =
+    fun { Context = (lineBreak, location); Target = target } ->
+        match (lineBreak, Seq.tryHead target) with
+        | (Crlf, Some '\r') ->
+            Ok {
+                Context = (Crlf, Location.carriageReturn location)
+                ParsedValue = '\r'
+                Rest = target[1..]
+            }
+        | (Crlf, Some '\n') ->
+            Ok {
+                Context = (Crlf, Location.lineFeed location)
+                ParsedValue = '\n'
+                Rest = target[1..]
+            }
+        | (Lf, Some '\n') ->
+            Ok {
+                Context = (Crlf, Location.nextLine location)
+                ParsedValue = '\n'
+                Rest = target[1..]
+            }
+        | (_, Some c') when c' = c ->
+            Ok {
+                Context = (lineBreak, Location.nextColumn location)
+                ParsedValue = c
+                Rest = target[1..]
+            }
+        | (_, Some c') ->
+            Error {
+                Context = (lineBreak, Location.nextColumn location)
+                Reason = $"char '%c{c}' expected, but char '%c{c'}' found"
+                Rest = target[1..]
+            }
+        | (_, None) ->
+            Error {
+                Context = (lineBreak, Location.nextColumn location)
+                Reason = $"char '%c{c}' expected, but reached end of input"
+                Rest = target[1..]
+            }
+
+let myParser =
+    charParser 'd'
+    |> Parser.map string
+    |> Parser.optional
+
+myParser { Context = (Lf, Location.root); Target = "def" }
+|> printfn "%A"
 
 let compileExpression (expression: Expression) =
     match expression with
@@ -75,40 +178,17 @@ let compileStatement (statement: Statement) =
     | VariableDeclaration { Identifier = identifier; Expression = expression } ->
         $"const %s{identifier.Raw} = %s{compileExpression(expression)};\n"
 
-let location: Location = {
-    Line = 0
-    Column = 0
-}
-
-let aParser: Parser<char, string> =
-    fun { Location = location; Target = target } ->
-        match Seq.tryHead target with
-        | Some 'a' ->
-            Ok {
-                Location = Location.nextColumn location
-                ParsedValue = 'a'
-                Rest = target[1..]
-            }
-        | Some c ->
-            Error {
-                Location = Location.nextColumn location
-                Message = $"char 'a' expected, but '%c{c}' found"
-                Rest = target[1..]
-            }
-        | None ->
-            Error {
-                Location = location
-                Message = "char 'a' expected, but reached end of input"
-                Rest = target
-            }
-
-aParser { Location = location; Target = "def" } |> printfn "%A"
-
 let statement = VariableDeclaration {
-    LetKeyword = { Location = location }
-    Identifier = { Location = location; Raw = "foo" }
-    Equal = { Location = location }
-    Expression = I32Literal { Location = location; Value = 42 }
+    LetKeyword = { Location = Location.root }
+    Identifier = {
+        Location = Location.root
+        Raw = "foo"
+    }
+    Equal = { Location = Location.root }
+    Expression = I32Literal {
+        Location = Location.root
+        Value = 42
+    }
 }
 
 printfn "%s" (compileStatement statement)
