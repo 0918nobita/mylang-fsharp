953772b25dce5256ddf3638168e5c263cf78c62e
Kodai Matsumoto <nobita.0918@gmail.com>
2023-09-09 11:32:54 +0900

    コード生成器を追加した

---
+++ src/App/CodeGen.fs
@@ -0,0 +1,25 @@
+module Mylang.CodeGen
+
+let rec private codeGenExpression (ast: Ast.Expression) : TSTree.Expression =
+    match ast with
+    | Ast.Identifier identifier -> TSTree.Identifier { Text = identifier.Raw }
+    | Ast.IntLiteral intLiteral -> TSTree.NumericLiteral { Text = intLiteral.Raw }
+    | Ast.CharLiteral charLiteral -> TSTree.StringLiteral { Text = string charLiteral.Raw }
+    | Ast.StringLiteral stringLiteral -> TSTree.StringLiteral { Text = stringLiteral.Raw }
+    | Ast.Funcall funcall ->
+        TSTree.CallExpression
+            { Callee = codeGenExpression funcall.Callee
+              Arguments = funcall.Arguments |> Array.map codeGenExpression }
+
+let codegen (ast: Ast.LetStmt[]) : TSTree.Program =
+    let stmts =
+        ast
+        |> Array.map (fun stmt ->
+            TSTree.VariableDeclarationList
+                { LetOrConst = TSTree.Const
+                  Declarations =
+                    [| { Identifier = { Text = stmt.Identifier.Raw }
+                         Type = None
+                         Initializer = codeGenExpression stmt.Initializer } |] })
+
+    { Body = stmts }
--- src/App/Program.fs
+++ src/App/Program.fs
@@ -1,24 +1,50 @@
 ﻿module Mylang.Program
 
+open System
 open System.IO
 open Parsec
+
+let exit1 errorMsg =
+    Console.ForegroundColor <- ConsoleColor.Red
+    eprintfn "%s" errorMsg
+    Console.ResetColor()
+    1
 
 [<EntryPoint>]
 let main argv =
     if Array.isEmpty argv then
-        printfn "ソースファイルを指定してください"
-        1
+        exit1 "ソースファイルを指定してください"
     else
         try
-            let source = File.ReadAllText argv.[0]
+            let sourceFileAttr = File.GetAttributes argv.[0]
 
-            use context = new ParserContext(source)
+            if (sourceFileAttr.HasFlag(FileAttributes.Directory)) then
+                exit1 $"ディレクトリが指定されました。ディレクトリではなくファイルを指定してください。(%s{argv.[0]})"
+            else
+                let sourceFileInfo = FileInfo argv.[0]
+                let sourceFile = sourceFileInfo.FullName
+                let outFileName = Path.ChangeExtension(sourceFileInfo.Name, ".ts")
+                let outFilePath = Path.Combine(sourceFileInfo.Directory.FullName, outFileName)
 
-            context.Run(MyParser.programParser, initialState = ()) |> printfn "%A"
+                Console.ForegroundColor <- ConsoleColor.Green
+                printf "[Compiling]"
+                Console.ResetColor()
+                printfn " %s => %s" sourceFile outFilePath
 
-            0
+                let source = File.ReadAllText sourceFile
+
+                use context = new ParserContext(source)
+
+                match context.Run(MyParser.programParser, initialState = ()) with
+                | Success success ->
+                    let parsed = success.ParsedValue |> List.toArray
+                    let tsAst = CodeGen.codegen parsed
+                    let compiled = tsAst |> TSTreePrinter.print
+                    File.WriteAllText(outFilePath, compiled + "\n")
+                    printfn "Done \u2728"
+                    0
+                | SoftFailure failure
+                | HardFailure failure -> exit1 $"%A{failure}"
         with
         | :? DirectoryNotFoundException
-        | :? FileNotFoundException ->
-            printfn "指定されたファイルが見つかりませんでした (%s)" argv.[0]
-            1
+        | :? FileNotFoundException -> exit1 $"指定されたファイルが見つかりませんでした (%s{argv.[0]})"
